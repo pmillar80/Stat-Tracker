a8478d98571e0b9523237863eb00b1c873ff7153
pmillar80 <elixx20@hotmail.com>
2025-11-13 00:30:59 -0600

    added status to header if not good centered text

--- site.js
+++ site.js
@@ -267,8 +267,8 @@ function createCharacterCard(character) {
         <div class="card-header">
             <div class="status-indicator ${statusClass}"></div>
             <div class="character-name">
-                <span>${character.Name || 'Unknown'}</span>
-                <span class="experience-preview">${character.Experience} / ${character.MaxExperience}</span>
+                <span class="name-text">${character.Name || 'Unknown'}</span>
+                <span class="experience-preview">${character.Status !== 'Good' ? `Status: ${character.Status}` : `XP: ${character.Experience} / ${character.MaxExperience}`}</span>
             </div>
         </div>
         <div class="card-body">
